8ea265ae60d9079787e14e798a26d526a5adf735
Samuel Legrain <samuel.legrain@bstorm.be>
2023-03-20 17:37:24 +0100

    Add CustomValidators

--- src/app/Components/create-event/create-event.component.ts
+++ src/app/Components/create-event/create-event.component.ts
@@ -1,3 +1,4 @@
+import { CustomValidatorsService } from './../../Services/custom-validators.service';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -9,19 +10,27 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CreateEventComponent implements OnInit {
   newEventForm! : FormGroup;
 
-  constructor(private _fb : FormBuilder){}
+  constructor(
+    private _fb : FormBuilder,
+    private _validators : CustomValidatorsService
+    ){}
 
   ngOnInit(): void {
     this.newEventForm = this._fb.group({
       name : [null, [Validators.required, Validators.minLength(2), Validators.maxLength(32)]],
       start_date : [null, [Validators.required]],
       end_date : [null, [Validators.required]],
-      is_all_day : [null, [Validators.required]],
+      is_all_day : [null, []], //Validators.required oblige que la case soit cochée...
       start_time : [null, []],
       end_time : [null, []],
       place : [null, [Validators.minLength(2), Validators.maxLength(64)]],
       participants : this._fb.array([])
-    });
+    },
+    //Ajout de validators pour l'ensemble du FormGroup
+    { validator:[
+      this._validators.checkDate(),
+      this._validators.checkTimeNeeded()
+    ]});
   }
 
   addParticipant():void{
